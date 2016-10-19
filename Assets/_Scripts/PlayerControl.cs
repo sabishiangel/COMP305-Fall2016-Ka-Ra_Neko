c1e814c53149e05cdb3189af48fabf53d0c1a9d5
sabishiangel <ashley.tjonhing@gmail.com>
2016-10-19 13:17:59 -0400

    initial commit

---
+++ Assets/_Scripts/PlayerControl.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+    //Private Instance Variables
+    private Transform _transform;
+    private Rigidbody2D _rigidbody;
+    private float _move;
+    private bool _isFacingLeft;
+    private bool _isGrounded;
+    private float _jump;
+    private SpriteRenderer spriteRender;
+
+    //Public Instance Variables
+    public Camera _camera;
+    public Transform spawnPoint;
+
+    //Public Properties
+    public float velocity = 10f;
+    public float jumpForce = 100f;
+
+	// Use this for initialization
+	void Start () {
+        this.initialize();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        if (this._isGrounded)
+        {
+            this._move = Input.GetAxis("Horizontal");
+            if (this._move > 0f)
+            {
+                this._move = 1f;
+                this.flip();
+                this._isFacingLeft = false;
+            }
+            else if (this._move < 0f)
+            {
+                this._move = -1f;
+                this.flip();
+                this._isFacingLeft = true;
+            }
+            else
+            {
+                this._move = 0f;
+            }
+            //Debug.Log (this._move);
+
+            //Jump Input
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                this._jump = 1;
+            }
+
+            if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+
+            }
+
+            //Movement
+            this._rigidbody.AddForce(new Vector2(this._move * Mathf.Clamp(this.velocity, 1f, 10f), this._jump * this.jumpForce), ForceMode2D.Force);
+
+        }
+        else
+        {
+            this._move = 0f;
+            this._jump = 0f;            
+        }
+
+        //Camera Movement
+        this._camera.transform.position = new Vector3(Mathf.Clamp(this._transform.position.x, 0f, 128.4f), Mathf.Clamp(this._transform.position.y, 0f, 6.56f), -10f);
+	}
+
+    private void initialize()
+    {
+        this._transform = GetComponent<Transform>();
+        this._rigidbody = GetComponent<Rigidbody2D>();
+        this._move = 0f;
+        this._isFacingLeft = true;
+        this._isGrounded = false;
+        this.spriteRender = GetComponent<SpriteRenderer>();
+    }
+
+    //Flips character direction
+    private void flip()
+    {
+        if (this._isFacingLeft)
+        {
+            spriteRender.flipX = false;
+        }
+        else
+        {
+            spriteRender.flipX = true;
+        }
+    }
+
+    //Causes events when player hits deathplane
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("DeathPlane"))
+        {
+            this._transform.position = this.spawnPoint.position;
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Platform"))
+            this._isGrounded = true;
+        Debug.Log(this._isGrounded);
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+            this._isGrounded = false;
+        Debug.Log(this._isGrounded);
+    }
+
+
+}
---
+++ Assets/_Scripts/SkyControl.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+/*Author: Ashley Tjon-Hing
+Date: October 17th 2016*/
+public class SkyControl : MonoBehaviour {
+    //Private Instance Variables
+    private Transform _transform;
+    public int speed = 1;
+
+    //Public Instance Variables
+
+
+    //Public Properties
+    public int Speed {
+        get { return this.speed; }
+        set { this.speed = value; }
+    } 
+
+	// Use this for initialization
+	void Start () {
+        this._transform = this.GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        StartCoroutine(Delay());
+        this.Boundary();
+	}
+
+    IEnumerator Delay()
+    {
+        this.Move();
+        yield return new WaitForSeconds(5);
+        this.Move();
+    }
+
+    //Method to move sky
+    private void Move()
+    {
+        Vector2 newPos = this._transform.position; //use old position to create new position
+        newPos.x -= (this.Speed); //create new pos
+        this._transform.position = newPos; //set new pos
+    }
+
+    //Prevent background from going off screen
+    private void Boundary()
+    {
+        if (this._transform.position.x <= -18.6)
+        { this.reset(); }
+    }
+
+    private void reset()
+    {
+        this._transform.position = new Vector2(18.6f, 0.28f);
+    }
+}
